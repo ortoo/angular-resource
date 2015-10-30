cd9d68cfc3dff4e26f9aa61f3d76e77f70566318
James Sharp <james@ortootech.com>
2015-10-30 10:24:08 +0000

    add in ngAnnotate for the build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
+var ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('default', function () {
   return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
       .pipe(babel())
+      .pipe(ngAnnotate({
+        regexp: /angular.*?\.module\(.*?\)$/
+      }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('lib'));
 });
