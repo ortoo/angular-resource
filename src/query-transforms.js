8b549f35182c243823f39140d76d0570cec7e9e0
James Sharp <james@ortootech.com>
2016-06-14 16:51:04 +0100

    add in a concat method for queries

--- src/query-transforms.js
+++ src/query-transforms.js
@@ -1,12 +1,14 @@
 var ArrayProto = Array.prototype;
 
 // Make the standard ES5 array transformations work as expected...
-export function apply(qry) {
+export function apply(qry, $q) {
 
   // These methods create new arrays
   ['filter', 'map', 'slice'].forEach(newArrayTransform);
 
   ['reverse', 'sort', 'splice'].forEach(inPlaceTransform);
+
+  qry.concat = concat;
 
   function inPlaceTransform(name) {
     qry[name] = function() {
@@ -39,5 +41,39 @@ export function apply(qry) {
 
       return newArr;
     };
+  }
+
+  function concat(...args) {
+    var newArr = [];
+    var qries = args.filter((obj) => (Array.isArray(obj) && obj.$emitter));
+    qries.push(qry);
+    qries.forEach((_qry) => {
+      _qry.$emitter.on('update', updateConcat);
+    });
+
+    updateConcat();
+
+    // Setup the next, prev etc
+    ['hasNext', 'hasPrev'].forEach(function(key) {
+      Object.defineProperty(newArr, key, {
+        get: function() {
+          return qries.some((_qry) => _qry[key]);
+        }
+      });
+    });
+
+    ['next', 'prev'].forEach(function(key) {
+      newArr[key] = function(...args) {
+        return $q.all(qries.map((_qry) => _qry[key](...args))).then(function() {
+          return newArr;
+        });
+      };
+    });
+
+    function updateConcat() {
+      var concatted = qry.concat(...args);
+      newArr.length = 0;
+      newArr.push(...concatted);
+    }
   }
 }
