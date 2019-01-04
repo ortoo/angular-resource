05e84010e8df1f8eccad3d30fbe8fcd29a5a4280
James Sharp <james@ortootech.com>
2019-01-04 10:49:08 +0000

    modernise build

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var sourcemaps = require('gulp-sourcemaps');
-var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
 var babelify = require('babelify');
 var through2 = require('through2');
@@ -22,32 +21,46 @@ var fileToString = through2.obj(function(file, enc, callback) {
   escapedContents = escapedContents.replace(/require/g, '__require');
 
   var newContents = `'format cjs';\nmodule.exports = ${escapedContents};`;
-  file.contents = new Buffer(newContents);
+  file.contents = Buffer.from(newContents);
   this.push(file);
   callback();
 });
 
-gulp.task('worker', function() {
+function worker() {
   return browserify({
     entries: 'src/db-worker.js'
-  }).transform(babelify, {presets: ['es2015']})
+  })
+    .transform(babelify, {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets:
+              'last 2 versions, not ie < 11, not ie_mob < 11, not android > 0'
+          }
+        ]
+      ]
+    })
     .bundle()
     .pipe(source('db-worker-string.js'))
     .pipe(buffer())
     .pipe(uglify())
     .pipe(fileToString)
     .pipe(gulp.dest('lib'));
-});
+}
 
-gulp.task('js', function() {
-  return gulp.src(['src/**/*.js'])
+function js() {
+  return gulp
+    .src(['src/**/*.js'])
     .pipe(sourcemaps.init())
-      .pipe(babel())
-      .pipe(ngAnnotate({
-        regexp: /angular.*?\.module\(.*?\)$/
-      }))
+    .pipe(
+      babel({
+        sourceType: 'module',
+        plugins: ['babel-plugin-angularjs-annotate']
+      })
+    )
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('lib'));
-});
+}
 
-gulp.task('default', ['js', 'worker']);
+module.exports.default = gulp.parallel(js, worker);
